7733b5f807dcce7833a4c2881a517b26ae7a5df7
Nathaly Victória <nathalyvictoria990@gmail.com>
2024-02-14 16:56:58 -0300

    feat: Adição das perguntas

--- js/main.js
+++ js/main.js
@@ -4,3 +4,25 @@ let pontuacao = 0;
 const elementoPergunta = document.getElementById("pergunta");
 const botoesResposta = document.getElementById("botoes-resposta");
 const botaoProximo = document.getElementById("proximo-botao");
+
+perguntas.push({
+    pergunta:'Qual o nome do melhor amigo de Chris?',
+    respostas:[
+        {texto:'Greg', acertou:true},
+        {texto:'Karuzo', acertou:false},
+        {texto:'Monk', acertou:false},
+        {texto:'Kill Moves', acertou:false},
+        {texto:'Joey', acertou:false},
+    ]
+});
+
+perguntas.push({
+    pergunta:'Quem é o narrador da série "Everybody Hates Chris"?',
+    respostas:[
+        {texto:'Terry Crews', acertou:false},
+        {texto:'Tichina Arnold', acertou:false},
+        {texto:'Tequan Richmond', acertou:false},
+        {texto:'Imani Hakim', acertou:false},
+        {texto:'Chris Rock', acertou:true},
+    ]
+});

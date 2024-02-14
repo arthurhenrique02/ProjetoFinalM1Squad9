4ad4544e7c4ff54f50a4c2ac5b2dafc397fee8d0
Samuel-Kepler <sr123456123@gmail.com>
2024-02-14 19:48:55 -0300

    feat: Implementação da função para mostrar perguntas

--- js/main.js
+++ js/main.js
@@ -8,21 +8,40 @@ const botaoProximo = document.getElementById("proximo-botao");
 perguntas.push({
     pergunta:'Qual o nome do melhor amigo de Chris?',
     respostas:[
-        {texto:'Greg', acertou:true},
-        {texto:'Karuzo', acertou:false},
-        {texto:'Monk', acertou:false},
-        {texto:'Kill Moves', acertou:false},
-        {texto:'Joey', acertou:false},
+        {texto:'Greg', acertou: true},
+        {texto:'Karuzo', acertou: false},
+        {texto:'Monk', acertou: false},
+        {texto:'Kill Moves', acertou: false},
+        {texto:'Joey', acertou: false},
     ]
 });
 
 perguntas.push({
     pergunta:'Quem é o narrador da série "Everybody Hates Chris"?',
     respostas:[
-        {texto:'Terry Crews', acertou:false},
-        {texto:'Tichina Arnold', acertou:false},
-        {texto:'Tequan Richmond', acertou:false},
-        {texto:'Imani Hakim', acertou:false},
-        {texto:'Chris Rock', acertou:true},
+        {texto:'Terry Crews', acertou: false},
+        {texto:'Tichina Arnold', acertou: false},
+        {texto:'Tequan Richmond', acertou: false},
+        {texto:'Imani Hakim', acertou: false},
+        {texto:'Chris Rock', acertou: true},
     ]
 });
+
+// Função para mostrar a pergunta atual
+function mostrarPergunta(perguntas, indicePerguntaAtual, elementoPergunta, botoesResposta) {
+    resetarEstado(botoesResposta, botaoProximo);
+    let perguntaAtual = perguntas[indicePerguntaAtual];
+    let numeroPergunta = indicePerguntaAtual + 1;
+    elementoPergunta.innerHTML = numeroPergunta + '. ' + perguntaAtual.pergunta;
+
+    perguntaAtual.respostas.forEach(resposta => {
+        const botao = document.createElement("button");
+        botao.innerHTML = resposta.texto;
+        botao.classList.add("botao");
+        botoesResposta.appendChild(botao);
+        if (resposta.acertou) {
+            botao.dataset.acertou = resposta.acertou;
+        }
+        botao.addEventListener("click", (e) => selecionarResposta(e, botoesResposta, botaoProximo));
+    });
+}

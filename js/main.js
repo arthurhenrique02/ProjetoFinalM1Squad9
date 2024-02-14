43c1a44190aa90c7b00f330f7253ee81c34f9519
Náteli <you@example.com>
2024-02-14 16:01:15 -0300

    feat: inicialização das variáveis

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const perguntas = [];
+let indicePerguntaAtual = 0;
+let pontuacao = 0;
+const elementoPergunta = document.getElementById("pergunta");
+const botoesResposta = document.getElementById("botoes-resposta");
+const botaoProximo = document.getElementById("proximo-botao");
